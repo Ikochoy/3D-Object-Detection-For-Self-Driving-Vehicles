fd6ac63548f6d67ff71a3a4ff632676ba9c34516
Ikochoy <kwan.choy@mail.utoronto.ca>
2022-02-24 00:27:43 -0500

    add statement for printing grid coords

--- detection/modules/loss_function.py
+++ detection/modules/loss_function.py
@@ -36,31 +36,31 @@ def heatmap_weighted_mse_loss(
     #l2_norm = torch.linalg.norm(predictions-targets, dim=1)
     #print(f"heaTMAP size: {heatmap.shape}")
     l2_norm = torch.sum(torch.square(predictions - targets), dim=1, keepdim=True).reshape(b, h, w)
-    print(f"l2norm\n{l2_norm}\n")
+    #print(f"l2norm\n{l2_norm}\n")
      # [batch_size x H x W] tensor `mse_loss`
     #l2_norm = torch.linalg.norm(predictions-targets, dim=1)
     #print(f"l2_norm size: {l2_norm.shape}")
     heatmap_reduced = torch.sum(heatmap, dim=1)  # reducing dimension
     #print(f"heatmap_reduced: {heatmap_reduced.shape}")
-    print(f"heatmap: \n{heatmap}\n")
-    print(f"heatmap_reduced: \n{heatmap_reduced}\n")
+    #print(f"heatmap: \n{heatmap}\n")
+    #print(f"heatmap_reduced: \n{heatmap_reduced}\n")
     mask = heatmap_reduced > heatmap_threshold # [batch_size x H x W] tensor `mask`
     #print(f"indices: \n {(mask).nonzero(as_tuple=False)}")
     heatmap_masked = torch.masked_select(heatmap_reduced, mask)  # 1-D tensor with only valid elements
-    print(f"heatmap_threshold: {heatmap_threshold}\n")
-    print(f"heatmap_masked: \n {heatmap_reduced[mask]}")
+    #print(f"heatmap_threshold: {heatmap_threshold}\n")
+    #print(f"heatmap_masked: \n {heatmap_reduced[mask]}")
     l2_norm_masked = torch.masked_select(l2_norm, mask)  # same order corresponding to heatmap_masked elements
     #print(torch.mean(heatmap_masked * l2_norm_masked))
     #print(torch.sum(heatmap_masked * l2_norm_masked)/(heatmap_masked.shape[0]))
-    print(f"l2norm_masked: \n {l2_norm[mask]}")
-    print(torch.mul(heatmap_masked, l2_norm_masked).sum()/(heatmap_masked.shape[0]))
+    #print(f"l2norm_masked: \n {l2_norm[mask]}")
+    #print(torch.mul(heatmap_masked, l2_norm_masked).sum()/(heatmap_masked.shape[0]))
     # torch.sum(
     #         heatmap * torch.square(predictions - targets)
     #         + torch.tensor(0, dtype=float),
     #         dim=1,
     #         keepdim=True,
     #     ).reshape(batch_size, H, W)
-    print(torch.mean(heatmap_masked * l2_norm_masked))
+    #print(torch.mean(heatmap_masked * l2_norm_masked))
     return torch.mean(heatmap_masked * l2_norm_masked) # average of element-wise multiplied masked entries
 
 
--- detection/modules/loss_target.py
+++ detection/modules/loss_target.py
@@ -110,7 +110,7 @@ class DetectionLossTargetBuilder:
         W_coords, H_coords = torch.arange(W), torch.arange(H)
         H_grid_coords, W_grid_coords = torch.meshgrid(H_coords, W_coords, indexing="ij")
         grid_coords = torch.stack([W_grid_coords, H_grid_coords], dim=-1)  # [H x W x 2]
-
+        print(f"grid coords: \n {grid_coords}\n")
         # 2. Create heatmap training targets by invoking the `create_heatmap` function.
         center = torch.tensor([cx, cy])
         scale = (x_size ** 2 + y_size ** 2) / self._heatmap_norm_scale
@@ -159,6 +159,10 @@ class DetectionLossTargetBuilder:
        # print(f"sin, cos\n {math.sin(yaw), math.cos(yaw)} \n headings \n {headings[index_mask.t()[0], index_mask.t()[1]]}\n")
         #print(headings)
         # 6. Concatenate training targets into a [7 x H x W] tensor.
+        print(heatmap[:, :, None].shape)
+        print(offsets.shape)
+        print(sizes.shape)
+        print(headings.shape)
         targets = torch.cat([heatmap[:, :, None], offsets, sizes, headings], dim=-1)
         return targets.permute(2, 0, 1)  # [7 x H x W]
 
